c85cf304da0c106df4bcb645bad27aea4b114f48
Bhargav158028 <bhargav15802808@gmail.com>
2026-06-10 20:16:53 +0530

    Update sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -4,7 +4,7 @@ cd /opt/
 sudo yum install unzip -y
 sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-8.9.6.50800.zip
 sudo unzip sonarqube-8.9.6.50800.zip
-sudo yum install java-21-amazon-corretto -y
+sudo yum install java-11-amazon-corretto -y
 sudo useradd sonar
 sudo chown -R sonar:sonar /opt/sonarqube-8.9.6.50800
 sudo chmod -R 775 /opt/sonarqube-8.9.6.50800 -R
